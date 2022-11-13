d0de7e307dfb72b666fcf12a53118b4b347cc430
Ed <edfuenzalidag@gmail.com>
2022-11-13 00:22:37 -0300

    Actualización Calculadora_fechas 2

--- Calculadora_fechas.sh
+++ Calculadora_fechas.sh
@@ -1,75 +1,97 @@
 #!/bin/bash
 
 PS3="
-Ingrese una opción: 
-"
+Ingrese una opción: "
 
-select opc in Sumar Restar Salir
+opciones=("Sumar" "Restar")
 
- do
-  case $opc in
+while true; do
 
-    Sumar)
-
-            echo 
-            read -p "Ingrese el número de días a avanzar: " di 
-            read -p "Ingrese el número de meses a avanzar: " me
-            read -p "Ingrese el número de años a avanzar: " ye
-            dat=$(date --date='+'"$di"' day +'"$me"' month +'"$ye"' year' +"%Y-%m-%d")
-            fecha=$(date)
-            
-            echo "---------------------------------------"
-            echo 
-            echo "Fecha actual: $fecha"
-            echo 
-            echo "Días a mover: +$di"
-            echo
-            echo "Meses a mover: +$me"
-            echo
-            echo "Años a mover: +$ye"
-            echo 
-            echo "Resultado de fecha modificada: $dat"
-            echo
-            cal "$(date --date='+'"$di"' day +'"$me"' month +'"$ye"' year' +"%Y-%m-%d")"
-            echo "---------------------------------------"
-    break;;
-
-    Restar)
-
-            echo 
-            read -p "Ingrese el número de días a retroceder: " di 
-            read -p "Ingrese el número de meses a retroceder: " me
-            read -p "Ingrese el número de años a retroceder: " ye
-            dat=$(date --date='-'"$di"' day -'"$me"' month -'"$ye"' year' +"%Y-%m-%d")
-            fecha=$(date)
-            
-            echo "---------------------------------------"
-            echo 
-            echo "Fecha actual: $fecha"
-            echo 
-            echo "Días a retroceder: -$di"
-            echo
-            echo "Meses a retroceder: -$me"
-            echo
-            echo "Años a retroceder: -$ye"
-            echo 
-            echo "Resultado de fecha modificada: $dat"
-            echo
-            cal "$(date --date='-'"$di"' day -'"$me"' month -'"$ye"' year' +"%Y-%m-%d")"
-            echo "---------------------------------------"
-
-    break;;
-
-    Salir)
-
-    break;;
-
-    *)
-
-        echo 
-        echo " Opción ingresada no valida"
-        echo " Favor ingresar una opciòn válida "    
         echo
-    
-    esac
-    done
+        echo "Calculadora para fechas"
+        echo
+
+        select opc in "${opciones[@]}" Salir
+
+        do
+        case $REPLY in
+
+        1)
+                echo 
+                read -p "Ingrese el número de días a avanzar: " di 
+                read -p "Ingrese el número de meses a avanzar: " me
+                read -p "Ingrese el número de años a avanzar: " ye
+                dat=$(date --date='+'"$di"' day +'"$me"' month +'"$ye"' year' +"%Y-%m-%d")
+                fecha=$(date)
+                
+                echo
+                echo "---------------------------------------"
+                echo 
+                echo "Fecha actual: $fecha"
+                echo 
+                echo "Días a mover: +$di"
+                echo
+                echo "Meses a mover: +$me"
+                echo
+                echo "Años a mover: +$ye"
+                echo 
+                echo "Resultado de fecha modificada: $dat"
+                echo
+                cal "$(date --date='+'"$di"' day +'"$me"' month +'"$ye"' year' +"%Y-%m-%d")"
+                echo
+                echo "---------------------------------------"
+        break;;
+
+        2)
+
+                echo 
+                read -p "Ingrese el número de días a retroceder: " di 
+                read -p "Ingrese el número de meses a retroceder: " me
+                read -p "Ingrese el número de años a retroceder: " ye
+                dat=$(date --date='-'"$di"' day -'"$me"' month -'"$ye"' year' +"%Y-%m-%d")
+                fecha=$(date)
+                
+                echo
+                echo "---------------------------------------"
+                echo 
+                echo "Fecha actual: $fecha"
+                echo 
+                echo "Días a retroceder: -$di"
+                echo
+                echo "Meses a retroceder: -$me"
+                echo
+                echo "Años a retroceder: -$ye"
+                echo 
+                echo "Resultado de fecha modificada: $dat"
+                echo
+                cal "$(date --date='-'"$di"' day -'"$me"' month -'"$ye"' year' +"%Y-%m-%d")"
+                echo
+                echo "---------------------------------------"
+
+        break;;
+
+        $((${#opciones[@]}+1)))
+
+                echo
+                echo "--------------------------"
+                echo
+                echo "Hasta pronto..."
+                echo
+                echo "---------------------------"
+                echo
+
+        break 2;;
+
+        *)
+                echo 
+                echo "-------------------------------------"
+                echo
+                echo " Opción ingresada no válida"
+                echo " Favor ingresar una opciòn válida "  
+                echo
+                echo "--------------------------------------"  
+                echo
+        
+        esac
+        done
+done        
