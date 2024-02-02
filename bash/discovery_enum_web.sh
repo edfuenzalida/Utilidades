31b77a2d015817fa465e0ac15d3eba33aeafb3fc
edfuenzalida <83800017+edfuenzalida@users.noreply.github.com>
2024-02-02 13:07:03 -0300

    Update discovery_enum_web.sh

--- bash/discovery_enum_web.sh
+++ bash/discovery_enum_web.sh
@@ -1,4 +1,12 @@
 #/bin/bash
+# Este script funciona para automatizar el uso de la herramienta de fuzzing fuff, desde el repositorio apt puede se instalada con el comando
+# sudo apt install fuff -y. 
+# El script toma una ruta de directorio en donde se encontrara 1 o más diccionarios a ser utilizados durante el fuzzing
+# leera todos uno por uno los diccionarios y para cada resultado, devolvera un archivo de texto el cual se guardado en la ruta predefinida
+# en la variable "save".
+# 
+# La estrucutra para lanzar el script es:
+# bash discovery_enum_web.sh ruta/diccionarios/ https://url.com/ejemplo ejemplo/resultadosfuff
 
 #Guarda la ruta de donde se extraeran los archivos de busqueda
 ruta=$1
